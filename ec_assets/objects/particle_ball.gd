be53900fd2248c5d4bcf81089e20bbc5ee49e4e7
flamango-juice <ethanalburo0221@gmail.ocm>
2026-02-19 17:08:53 -0800

    fully added electron affinity code

--- ec_assets/objects/particle_ball.gd
+++ ec_assets/objects/particle_ball.gd
@@ -4,15 +4,18 @@ enum particle_type {ELECTRON, POSITRON}
 var dragged:bool = false
 var sound_played:bool = false
 var balltype:int = 0
-
+var attract = null
+var repel = null
 func set_appearance(type):
 	match type:
 		particle_type.ELECTRON:
 			modulate = Color.WHITE
 			$positron.visible = false
+			$electron.visible = true
 		particle_type.POSITRON:
 			modulate = Color.RED
 			$electron.visible = false
+			$positron.visible = true
 		
 
 func _ready() -> void:
@@ -43,9 +46,21 @@ func _physics_process(delta: float) -> void:
 	rotation += velocity.x / 1000
 	
 	velocity.y += glob.gravity*100 * delta * glob.rate
-	#prints(glob.gravity*100 * delta * glob.rate, velocity.y)
+
+	if balltype == particle_type.ELECTRON:
+		if attract != null:
+			velocity.x -= (cos((position-attract.position).angle()) * 100 / position.distance_squared_to(attract.position) * glob.rate) * absf(attract.affinity)
+			velocity.y -= (sin((position-attract.position).angle()) * 100 / position.distance_squared_to(attract.position) * glob.rate) * absf(attract.affinity)
+
+		if repel != null:
+			velocity.x += (cos((position-repel.position).angle()) * 100 / position.distance_squared_to(repel.position) * glob.rate) * absf(repel.affinity)
+			velocity.y += (sin((position-repel.position).angle()) * 100 / position.distance_squared_to(repel.position) * glob.rate) * absf(repel.affinity)
+
 	var collision = move_and_collide(velocity * delta)
 	if collision:
+		if collision.get_collider() is element_catto:
+			var col = collision.get_collider()
+			col.velocity = velocity / 100
 		velocity = velocity.bounce(collision.get_normal())
 		velocity.y /= 1.1
 
@@ -54,7 +69,16 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	glob.selected = null
 
-
-func _on_hitbox_body_entered(body: Node2D) -> void:
+func _on_range_body_entered(body: Node2D) -> void:
 	if body is element_catto:
-		velocity.y = body.velocity.y
+		if balltype == particle_type.ELECTRON:
+			match int(sign(body.affinity)):
+				-1: repel = body
+				1: attract = body
+
+func _on_range_body_exited(body: Node2D) -> void:
+	if body is element_catto:
+		if balltype == particle_type.ELECTRON:
+			match int(sign(body.affinity)):
+				-1: repel = null
+				1: attract = null
